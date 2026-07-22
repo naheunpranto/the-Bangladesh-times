9e0f7b36d595e26fa53b75a566a1a5086895808d
Naheun Alam <naheunpranto258@gmail.com>
2026-07-23 00:21:20 +0600

    Display Categories & Design Sidebar

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,3 +1,6 @@
+import LeftSidebar from "@/components/Homepage/news/LeftSidebar";
+import RightSidebar from "@/components/Homepage/news/RightSidebar";
+
  
 
 
@@ -17,22 +20,15 @@ export default async function Home() {
   return (
     <div className="grid grid-cols-12 gap-4 container mx-auto my-[60px]">
       <div className=" col-span-3">
-        <h2 className="font-semibold text-[20px]">All categories</h2>
-        <ul className="flex flex-col gap-3 mt-[20px]">
-          {
-            categories.news_category.map(category => {
-              return <li key={category.id} className="bg-slate-100 p-2 rounded-md font-semibold text-center text-[20px]">{category.category_name}</li>
-            })
-          }
-        </ul>
+        <LeftSidebar categories={categories} activeId={null}/>
       </div>
 
       <div className="font-bold text-3xl bg-purple-500 col-span-6">
         Dragon News
       </div>
 
-      <div className="font-bold text-3xl bg-yellow-500 col-span-3">
-        Login with
+      <div className="col-span-3">
+        <RightSidebar/>
       </div>
 
     </div>
---
+++ src/components/Homepage/news/LeftSidebar.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const LeftSidebar = ({categories, activeId}) => {
+  return (
+    <div>
+      <h2 className="font-semibold text-[20px]">All categories</h2>
+      <ul className="flex flex-col gap-3 mt-[20px]">
+        {categories.news_category.map((category) => {
+          return (
+            <li
+              key={category.category_id}
+              className={`${activeId === category.category_id && "bg-slate-100"}  rounded-md font-semibold text-center text-[20px]`}
+            >
+              <Link href={`/category/${category.category_id}`} className="block p-2">{" "}{category.category_name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default LeftSidebar;
